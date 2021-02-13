b029a0004597f3d6a29eb1c8a8b43a964ee2bf1e
Jung Huh <jhuh3226@gmail.com>
2021-02-13 11:45:18 -0500

    error solved

--- class1/connectedDevice_button1/connectedDevice_button1.ino
+++ class1/connectedDevice_button1/connectedDevice_button1.ino
@@ -1,8 +1,11 @@
 #include <Arduino_JSON.h>
 
 const int buttonPin = 3;
-int lastButtonState = 0;
+int lastButtonState = LOW;
 // volatile makes sure
+
+// if the inputs have changed:
+bool inputsChanged = false;
 
 JSONVar outgoing;
 
@@ -26,8 +29,8 @@ void setup() {
 
 void loop() {
   //if there's no serial go back to starting loop
-  if(!Serial) return;
-  
+  if (!Serial) return;
+
   // put your main code here, to run repeatedly:
   int buttonState = digitalRead(buttonPin);
 
@@ -38,11 +41,20 @@ void loop() {
     delay(5);
     //Serial.println(buttonState);
 
-   //if(Serial) Serial.println(button);
-    if(Serial) Serial.print(JSON.stringify(outgoing));
-    if(Serial) Serial.print(",");
+    //if(Serial) Serial.println(button);
+    //if(Serial) Serial.println(JSON.stringify(outgoing));
+    //if(Serial) Serial.print(",");
 
     lastButtonState = buttonState;
+    // set change flag so serial will send:
+    inputsChanged = true;
+  }
+
+  // if either sensor has changed, send it:
+  if (inputsChanged) {
+    Serial.println(outgoing);
+    // clear the change flag:
+    inputsChanged = false;
   }
 
 }
